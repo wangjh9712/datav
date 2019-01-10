f8167afe6ec71c0ad0c60ce0e422c2bbce4b2b9b
Jyanhwei <wangjh9712@gmail.com>
2019-01-10 10:32:58 +0800

    ch

---
+++ component/header.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">全国主要城市数据展现</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">主页</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="map.php">地图</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="popu.php">人口</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="gdp.php">GDP</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="housepay.php">房价</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      所有统计数据均来自于<a class="" href="http://data.stats.gov.cn/" target="_blank">国家统计局</a>
+    </span>
+  </div>
+</nav>
